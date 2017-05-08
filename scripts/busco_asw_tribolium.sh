4e8d53b8e7d8f6319c210ce597b8c3361a7910ab
Sarah Inwood <sninwood@gmail.com>
2017-05-08 12:56:21 +1200

    edited script to run on binned SOAP 51 assembly

--- scripts/busco_asw_tribolium.sh
+++ scripts/busco_asw_tribolium.sh
@@ -3,8 +3,8 @@ export PATH="${HOME}/local/bin:${HOME}/local/bin/augustus/bin:${HOME}/local/bin/
 export AUGUSTUS_CONFIG_PATH="${HOME}/local/bin/augustus/config"
 
 python bin/busco/BUSCO.py \
---in data/assemblies/nopcr.binned_meraculous_51.fa \
---out nopcr.binned_meraculous_51_tribolium \
+--in data/assemblies/nopcr.binned_SOAPdenovo2_51.fa \
+--out nopcr.binned_SOAP_51_tribolium \
 --lineage data/endopterygota_odb9 \
 --mode genome \
 --cpu 10 --species tribolium2012
